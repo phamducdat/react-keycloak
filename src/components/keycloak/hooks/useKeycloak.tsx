633bcc70e92741519f4d94636fe7e4b0ea280992
phamducdat <phamducdat2402@gmail.com>
2023-05-02 18:05:13 +0700

    fix(useKeycloak): using loadUserInfo

--- src/components/keycloak/hooks/useKeycloak.tsx
+++ src/components/keycloak/hooks/useKeycloak.tsx
@@ -3,14 +3,28 @@ import { useKeycloak as originalUseKeycloak } from '@react-keycloak/web';
 
 const useKeycloak = () => {
   const { initialized, keycloak, ...rest } = originalUseKeycloak();
-  const customLogin = () => {
-    console.log('dat with before login');
-    keycloak.login().then((response) => {
-      console.log('dat with keycloak = ', keycloak);
-      console.log('dat with response in login = ', response);
-    });
-    console.log('dat with after login');
+
+  const checkAccess = (userInfo: any) => {
+    console.log('User information:', userInfo);
+    // Perform your access check based on the user information
   };
+
+  const customLogin = async () => {
+    console.log('dat with before login');
+
+    try {
+      await keycloak.login();
+      console.log('dat with keycloak = ', keycloak);
+
+      const userInfo = await keycloak.loadUserInfo();
+      checkAccess(userInfo); // Perform the check after the login process is complete
+    } catch (error) {
+      console.error('Error during login:', error);
+    } finally {
+      console.log('dat with after login');
+    }
+  };
+
   return {
     initialized,
     keycloak: {

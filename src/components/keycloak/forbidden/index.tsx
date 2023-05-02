ce8ad56dd54ae5cc577004e1aba993d39fba111b
phamducdat <phamducdat2402@gmail.com>
2023-05-02 17:42:52 +0700

    fix(useKeycloak): add logs to check login

--- src/components/keycloak/forbidden/index.tsx
+++ src/components/keycloak/forbidden/index.tsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchData } from './api';
 
 const Forbidden: React.FC = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    async function fetchAndSetData() {
+      const url = `https://datpd.free.beeceptor.com/`;
+      const fetchedData = await fetchData(url);
+      setData(fetchedData);
+    }
+
+    fetchAndSetData();
+  }, []);
+
+  console.log('dat with fetched data = ', data);
+
   return <div>Forbidden</div>;
 };
 
--- src/components/keycloak/hooks/useKeycloak.tsx
+++ src/components/keycloak/hooks/useKeycloak.tsx
@@ -4,9 +4,12 @@ import { useKeycloak as originalUseKeycloak } from '@react-keycloak/web';
 const useKeycloak = () => {
   const { initialized, keycloak, ...rest } = originalUseKeycloak();
   const customLogin = () => {
+    console.log('dat with before login');
     keycloak.login().then((response) => {
+      console.log('dat with keycloak = ', keycloak);
       console.log('dat with response in login = ', response);
     });
+    console.log('dat with after login');
   };
   return {
     initialized,

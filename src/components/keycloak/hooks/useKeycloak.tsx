423f222441c0d80fd391c6ec1e8c58bdc18d9a44
phamducdat <phamducdat2402@gmail.com>
2023-05-02 18:47:42 +0700

    feat(useKeycloak): add permission state

--- src/components/keycloak/hooks/useKeycloak.tsx
+++ src/components/keycloak/hooks/useKeycloak.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useKeycloak as originalUseKeycloak } from '@react-keycloak/web';
 
 const useKeycloak = () => {
   const { initialized, keycloak, ...rest } = originalUseKeycloak();
+  const [permission, setPermission] = useState(false);
 
   const checkAccess = (userInfo: any) => {
     console.log('User information:', userInfo);
     // Perform your access check based on the user information
   };
 
+  const handleAuthSuccess = async () => {
+    setPermission(false);
+    console.log('Authentication successful');
+    const userInfo = await keycloak.loadUserInfo();
+    localStorage.setItem('userInfo', JSON.stringify(userInfo));
+    checkAccess(userInfo);
+  };
+
+  useEffect(() => {
+    keycloak.onAuthSuccess = handleAuthSuccess;
+
+    return () => {
+      keycloak.onAuthSuccess = undefined;
+    };
+  }, [keycloak]);
   const customLogin = async () => {
     console.log('dat with before login');
-    localStorage.setItem('datpd', '100');
 
     try {
       await keycloak.login();
       console.log('dat with keycloak = ', keycloak);
 
       const userInfo = await keycloak.loadUserInfo();
-      localStorage.setItem('userInfo', JSON.stringify(userInfo));
       checkAccess(userInfo); // Perform the check after the login process is complete
     } catch (error) {
       console.error('Error during login:', error);
@@ -32,6 +46,7 @@ const useKeycloak = () => {
     keycloak: {
       ...keycloak,
       login: customLogin,
+      permission: permission,
     },
     ...rest,
   };
